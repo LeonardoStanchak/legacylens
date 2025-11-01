062a82061a694e16a4a413bfe4e74b2ebbf2c455
Leonardo Stanchak <leonardostanchak@hotmail.com>
2025-11-01 20:30:35 -0300

    feature: criação do esqueleto legacylens

---
+++ src/main/java/br/com/legacylens/LegacylensApplication.java
@@ -0,0 +1,13 @@
+package br.com.legacylens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacylensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacylensApplication.class, args);
+	}
+
+}
